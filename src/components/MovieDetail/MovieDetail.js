c18e1d3a4e4a97f4e9caa9cba834199b6040f54d
JasonYen <jason753357@gmail.com>
2021-10-13 13:34:46 +0800

    css修改

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAsyncDetail,removeSelected } from "../../features/movies/movieSlice";
 import "./MovieDetail.scss";
+import { FaStar, FaThumbsUp, FaClock, FaCalendarAlt} from "react-icons/fa";
 
 const MovieDetail = () => {
   const { imdbID } = useParams();
@@ -19,16 +20,16 @@ const MovieDetail = () => {
   return (
     <div className="movie-section">
         {Object.keys(data).length === 0?
-        (<div>. . .LOADING</div>) :(  
+        (<div className="loadingdiv">. . . LOADING</div>) :(  
     
         <>
       <div className="section-left">
         <div className="movie-title">{data.Title}</div>
         <div className="movie-rating">
-          <span>IMDB Rating : {data.imdbRating}</span>
-          <span>IMDB Votes : {data.imdbVotes}</span>
-          <span>RunTime : {data.imdbRating}</span>
-          <span>Year : {data.Year}</span>
+          <span><FaStar className="star"/> Rating : {data.imdbRating}</span>
+          <span><FaThumbsUp className="thumb"/> Votes : {data.imdbVotes}</span>
+          <span><FaClock className="clock"/> RunTime : {data.Runtime}</span>
+          <span><FaCalendarAlt className="year"/> Year : {data.Year}</span>
         </div>
         <div className="movie-plot">{data.Plot}</div>
         <div className="movie-info">
